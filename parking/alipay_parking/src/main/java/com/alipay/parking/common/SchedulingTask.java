cf564099ad6a64e925af5a32bc9e71009bf03339
liuzhuang <liuzhuangjob@163.com>
2019-07-06 18:45:08 +0800

    更换parking-core版本

--- parking/alipay_parking/src/main/java/com/alipay/parking/common/SchedulingTask.java
+++ parking/alipay_parking/src/main/java/com/alipay/parking/common/SchedulingTask.java
@@ -13,28 +13,28 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @应用场景：对代扣结果未知的订单做一个定时任务，去查询订单是否成功扣款，如果扣款成功并且线下支付过的，需要同步订单，然后把代扣的钱退回用户
  * @date 2019年6月18日
  */
-@Configuration
-@EnableScheduling
+//@Configuration
+//@EnableScheduling
 public class SchedulingTask {
 
 	/**
 	 * @Description:查询订单结果，处理相关逻辑
 	 * @date 2019年6月18日
 	 */
-	public void queryOrderResult() {
-		TimerTask task = new TimerTask() {
-			@Override
-			public void run() {
-				// TODO
-				// 对于失败的订单可以定期的执行
-				// 可以把结果未知的订单放到一个列表中，定时执行一下，先判断列表不为空时去查询结果，当结果明确时调用订单同步接口把结果同步到停车平台，展示给用户记录信息
-			}
-		};
-		Timer timer = new Timer();
-		long delay = 0;// 任务执行前的毫秒延迟
-		long intevalPeriod = 1 * 1000;// 执行间隔时间
-		// schedules the task to be run in an interval
-		// task待执行的任务
-		timer.scheduleAtFixedRate(task, delay, intevalPeriod);
-	}
+//	public void queryOrderResult() {
+//		TimerTask task = new TimerTask() {
+//			@Override
+//			public void run() {
+//				// TODO
+//				// 对于失败的订单可以定期的执行
+//				// 可以把结果未知的订单放到一个列表中，定时执行一下，先判断列表不为空时去查询结果，当结果明确时调用订单同步接口把结果同步到停车平台，展示给用户记录信息
+//			}
+//		};
+//		Timer timer = new Timer();
+//		long delay = 0;// 任务执行前的毫秒延迟
+//		long intevalPeriod = 1 * 1000;// 执行间隔时间
+//		// schedules the task to be run in an interval
+//		// task待执行的任务
+//		timer.scheduleAtFixedRate(task, delay, intevalPeriod);
+//	}
 }
